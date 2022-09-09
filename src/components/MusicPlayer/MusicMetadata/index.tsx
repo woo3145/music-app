bf4c7b1d972cfd959c47adcdc7807193b9f22d7c
woo3145 <lcwoo3145@gmail.com>
2022-09-09 23:24:41 +0900

    Work - MusicPlayer UI 마무리 (메타데이터 추가)

--- src/components/MusicPlayer/MusicMetadata/index.tsx
+++ src/components/MusicPlayer/MusicMetadata/index.tsx
@@ -1,5 +1,35 @@
+import { RiPlayListLine } from 'react-icons/ri';
+import { useAppSelector } from '../../../utils/redux/store';
+
 const MusicMetadata = () => {
-  return <div>MusicMetadata</div>;
+  const currentTrack = useAppSelector((state) => state.playlist.currentTrack);
+
+  if (!currentTrack) {
+    return null;
+  }
+  return (
+    <div className="shrink-0 w-60 flex items-center justify-between">
+      <div className="flex items-center">
+        {/* artwork */}
+        <div className="w-9 shrink-0">
+          <img src={currentTrack.artworkUrl} alt="" className="rounded-md" />
+        </div>
+        {/* title & artistName */}
+        <div className="px-3">
+          <p className="text-xs leading-4 opacity-60 cursor-pointer hover:opacity-100 duration-200 break-all line-clamp-1">
+            {currentTrack.artistName}
+          </p>
+          <p className="text-sm leading-4 opacity-70 cursor-pointer hover:opacity-100 duration-200 break-all line-clamp-1">
+            {currentTrack.name}
+          </p>
+        </div>
+      </div>
+      {/* menu */}
+      <div className="flex">
+        <RiPlayListLine className="text-lg opacity-70 cursor-pointer hover:opacity-100 duration-200" />
+      </div>
+    </div>
+  );
 };
 
 export default MusicMetadata;
