2de59fe05147b882ef3dea68121ce2cdaea7e6e9
woo3145 <lcwoo3145@gmail.com>
2022-09-14 20:54:33 +0900

    Refactor - PlaylistItem 분리, Docs - 버그 추가

---
+++ src/components/MusicPlayer/PlaylistPopup/PlaylistItem/index.tsx
@@ -0,0 +1,71 @@
+import { BsPlayFill } from 'react-icons/bs';
+import { IoClose } from 'react-icons/io5';
+import { Link } from 'react-router-dom';
+import {
+  deleteTrack,
+  selectTrackInPlaylist,
+} from '../../../../utils/redux/modules/playlistSlice';
+import { useAppDispatch } from '../../../../utils/redux/store';
+
+interface Props {
+  track: ITrack;
+  idx: number;
+  selected: boolean;
+}
+const PlaylistItem = ({ track, idx, selected }: Props) => {
+  const dispatch = useAppDispatch();
+
+  const onDeleteTrack = () => {
+    dispatch(deleteTrack(idx));
+  };
+
+  const onPlayTrack = () => {
+    dispatch(selectTrackInPlaylist(idx));
+  };
+
+  return (
+    <li
+      className={`px-4 py-2 ${
+        selected ? 'bg-slate-200' : 'hover:bg-slate-200'
+      }`}
+    >
+      <div className="group flex items-center cursor-pointer">
+        <div
+          className="relative shrink-0 flex items-center justify-center"
+          onClick={onPlayTrack}
+        >
+          <img
+            src={track.artworkUrl}
+            alt="avator"
+            className="w-10 h-auto cursor-pointer group-hover:brightness-75 duration-200 rounded-md"
+          />
+          <BsPlayFill
+            className={`text-white absolute ${
+              selected ? '' : 'invisible group-hover:visible'
+            }`}
+          />
+        </div>
+        <div className="px-4 w-full">
+          <Link
+            to=""
+            className="block text-sm hover:underline line-clamp-1 md:line-clamp-2"
+          >
+            {track.name}
+          </Link>
+          <Link
+            to=""
+            className="block text-xs opacity-60 hover:underline line-clamp-1 md:line-clamp-2"
+          >
+            {track.artistName}
+          </Link>
+        </div>
+        <IoClose
+          onClick={onDeleteTrack}
+          className="text-2xl text-blue-900 cursor-pointer shrink-0 invisible group-hover:visible opacity-70 hover:opacity-100"
+        />
+      </div>
+    </li>
+  );
+};
+
+export default PlaylistItem;
--- src/components/MusicPlayer/PlaylistPopup/index.tsx
+++ src/components/MusicPlayer/PlaylistPopup/index.tsx
@@ -1,12 +1,7 @@
-import { BsPlayFill } from 'react-icons/bs';
 import { IoClose } from 'react-icons/io5';
-import { Link } from 'react-router-dom';
-import {
-  clearPlaylist,
-  deleteTrack,
-  selectTrackInPlaylist,
-} from '../../../utils/redux/modules/playlistSlice';
+import { clearPlaylist } from '../../../utils/redux/modules/playlistSlice';
 import { useAppDispatch, useAppSelector } from '../../../utils/redux/store';
+import PlaylistItem from './PlaylistItem';
 
 interface PlaylistPopupProps {
   playlistToggle: () => void;
@@ -41,7 +36,7 @@ const PlaylistPopup = ({ playlistToggle }: PlaylistPopupProps) => {
         {playlist &&
           playlist.tracks.map((track, idx) => {
             return (
-              <PlaylistTrackCard
+              <PlaylistItem
                 idx={idx}
                 key={idx}
                 track={track}
@@ -51,71 +46,6 @@ const PlaylistPopup = ({ playlistToggle }: PlaylistPopupProps) => {
           })}
       </ul>
     </div>
-  );
-};
-
-interface PlaylistTrackCardProps {
-  track: ITrack;
-  idx: number;
-  selected: boolean;
-}
-const PlaylistTrackCard = ({
-  track,
-  idx,
-  selected,
-}: PlaylistTrackCardProps) => {
-  const dispatch = useAppDispatch();
-
-  const onDeleteTrack = () => {
-    dispatch(deleteTrack(idx));
-  };
-
-  const onPlayTrack = () => {
-    dispatch(selectTrackInPlaylist(idx));
-  };
-
-  return (
-    <li
-      className={`px-4 py-2 ${
-        selected ? 'bg-slate-200' : 'hover:bg-slate-200'
-      }`}
-    >
-      <div className="group flex items-center cursor-pointer">
-        <div
-          className="relative shrink-0 flex items-center justify-center"
-          onClick={onPlayTrack}
-        >
-          <img
-            src={track.artworkUrl}
-            alt="avator"
-            className="w-10 h-auto cursor-pointer group-hover:brightness-75 duration-200 rounded-md"
-          />
-          <BsPlayFill
-            className={`text-white absolute ${
-              selected ? '' : 'invisible group-hover:visible'
-            }`}
-          />
-        </div>
-        <div className="px-4 w-full">
-          <Link
-            to=""
-            className="block text-sm hover:underline line-clamp-1 md:line-clamp-2"
-          >
-            {track.name}
-          </Link>
-          <Link
-            to=""
-            className="block text-xs opacity-60 hover:underline line-clamp-1 md:line-clamp-2"
-          >
-            {track.artistName}
-          </Link>
-        </div>
-        <IoClose
-          onClick={onDeleteTrack}
-          className="text-2xl text-blue-900 cursor-pointer shrink-0 invisible group-hover:visible opacity-70 hover:opacity-100"
-        />
-      </div>
-    </li>
   );
 };
 
